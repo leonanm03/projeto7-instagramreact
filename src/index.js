8d4a24a6d48fcd5610996141972246ee82ffc08f
Leonan <leonanm04@gmail.com>
2022-11-15 14:48:35 -0300

    reset e css inicializados

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+
+
+
+function Teste() {
+    return (
+        <h1>Instagram React</h1>
+    )
+}
+
+
+const elemento = document.querySelector(".root");
+ReactDOM.render(Teste(), elemento);
